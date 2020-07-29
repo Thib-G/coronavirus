906ff98fefc54f827b1b595034f478ea55791100
Thib <thibaut.g@gmail.com>
2020-07-29 21:59:17 +0200

    Add absolute path

--- data/convert_muni.py
+++ data/convert_muni.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
 import pandas as pd
 from datetime import date, timedelta
 
-df = pd.read_json('COVID19BE_CASES_MUNI.json', encoding='utf-8', orient='records', dtype={'NIS5': str, 'CASES': int})
+path = os.path.dirname(__file__)
+
+print(path)
+
+df = pd.read_json(os.path.join(path, 'COVID19BE_CASES_MUNI.json'), encoding='utf-8', orient='records', dtype={'NIS5': str, 'CASES': int})
 
 df.set_index('DATE', inplace=True)
 df['CASES'] = pd.to_numeric(df['CASES'], errors='coerce')
+
+print(os.getcwd())
 
 def sum_last_days(dataframe, days=14):
     cols = dataframe.columns.to_list()
@@ -17,5 +24,5 @@ def sum_last_days(dataframe, days=14):
     df_grouped = df_sliced.groupby(cols, as_index=False).sum()
     return df_grouped
 
-sum_last_days(df, days=7).to_json('COVID19BE_CASES_MUNI_7DAYS.json', orient='records')
-sum_last_days(df, days=14).to_json('COVID19BE_CASES_MUNI_14DAYS.json', orient='records')
+sum_last_days(df, days=7).to_json(os.path.join(path, 'COVID19BE_CASES_MUNI_7DAYS.json'), orient='records')
+sum_last_days(df, days=14).to_json(os.path.join(path, 'COVID19BE_CASES_MUNI_14DAYS.json'), orient='records')
